bfd31d4990687b4899f2c7c54fe0d5be5597b6ff
Yasen Sotirov <ia.sotirov@gmail.com>
2023-08-11 19:07:16 +0300

    Ime na commitIme na commitIme na commitaaa

---
+++ main.py
@@ -0,0 +1,15 @@
+class Laptop:
+    made_in = "Germany"
+
+    def __init__(self, memory:int, model:str):
+        self.memory = memory
+        self.model = model
+
+    @classmethod    # създава точно определена инстанция на класа
+    def low_ram_laptop(cls):
+        return cls(256, "Asus")
+
+    @classmethod
+    def made_in_other(cls):
+        cls.made_in = "China"
+        return f"{cls.made_in}"
