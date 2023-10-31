7f22b6db92bda0e264732a1a9d16f68606ea6df9
Yasen Sotirov <ia.sotirov@gmail.com>
2023-10-31 12:11:55 +0200

    #1 start

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-class Laptop:
-    made_in = "Germany"
-
-    def __init__(self, memory:int, model:str, price: float):
-        self.memory = memory
-        self.model = model
-        self.price = price
-
-    @classmethod    # създава точно определена инстанция на класа
-    def low_ram_laptop(cls):
-        return cls(256, "Asus")
-
-    @classmethod
-    def made_in_other(cls):
-        cls.made_in = "Neverland"
-        return f"{cls.made_in}"
+def addition(a, b):
+    return a + b
